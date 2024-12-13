bbf5239b25a4d6ce1f6962c67fa95317a3767f5b
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-12-13 15:39:01 -0300

    first frontend part ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <h1>
-      Welcome to Proshop
-    </h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+         <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+const Rating = ({ value, text }) => {
+  return (
+    <div className="rating">
+      {/* 1 start */}
+      <span>
+        {value >= 1 ? (
+          <FaStar />
+        ) : value >= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+
+      {/* 2 start */}
+      <span>
+        {value >= 2 ? (
+          <FaStar />
+        ) : value >= 1.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+
+      {/* 3 start */}
+      <span>
+        {value >= 3 ? (
+          <FaStar />
+        ) : value >= 2.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+
+      {/* 4 start */}
+      <span>
+        {value >= 4 ? (
+          <FaStar />
+        ) : value >= 3.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+
+      {/* 5 start */}
+      <span>
+        {value >= 5 ? (
+          <FaStar />
+        ) : value >= 4.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+
+      <span className="rating-text">{text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
