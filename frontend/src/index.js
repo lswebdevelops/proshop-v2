cebc3b601d28b4f43ea78b15f4982387d4a89890
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-12-23 13:47:47 -0300

    added redux kit to HomeScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,24 +9,29 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "./index.css";
 import "./assets/styles/index.css";
 import "./assets/styles/bootstrap.custom.css";
 import App from "./App";
 
-
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route path="/" element={<App />}>
-    <Route index={true} path="/" element={<HomeScreen />} />
-    <Route  path="/product/:id" element={<ProductScreen />} />
-  </Route>)
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
