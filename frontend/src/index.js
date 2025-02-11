beb024ec999a22952f06b7a7780ead2783e76db2
lswebdevelops <sardanhaluciano9690@gmail.com>
2025-02-11 15:21:53 -0300

    added payment method to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,25 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import PrivateRoute from "./components/PrivateRoute";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
-
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import {  createBrowserRouter,  createRoutesFromElements,  Route,  RouterProvider,} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "./index.css";
 import "./assets/styles/index.css";
 import "./assets/styles/bootstrap.custom.css";
 import App from "./App";
+import PaymentScreen from "./screens/PaymentScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -30,8 +23,11 @@ const router = createBrowserRouter(
       <Route path="/cart/" element={<CartScreen />} />
       <Route path="/login/" element={<LoginScreen />} />
       <Route path="/register/" element={<RegisterScreen />} />
-      <Route path="/shipping/" element={<ShippingScreen />} />
 
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping/" element={<ShippingScreen />} />
+        <Route path="/payment/" element={<PaymentScreen />} />
+      </Route>
     </Route>
   )
 );
