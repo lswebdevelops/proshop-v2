2befc1d1ac1c135299ca3d6d8842c7a7d7e10333
lswebdevelops <sardanhaluciano9690@gmail.com>
2025-02-10 21:02:55 -0300

    adding shipping screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 import {
   createBrowserRouter,
@@ -29,6 +30,7 @@ const router = createBrowserRouter(
       <Route path="/cart/" element={<CartScreen />} />
       <Route path="/login/" element={<LoginScreen />} />
       <Route path="/register/" element={<RegisterScreen />} />
+      <Route path="/shipping/" element={<ShippingScreen />} />
 
     </Route>
   )
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [] };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -27,11 +27,15 @@ const cartSlice = createSlice({
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
 
-    // updating localstorage
-      return  updateCart(state)
+      // updating localstorage
+      return updateCart(state);
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
     },
   },
 });
 
-export const { addToCart , removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions;
 export default cartSlice.reducer;
