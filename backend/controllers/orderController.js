46d4efc1a7a1abc47590d5ccefaa9caf14ecccdb
lswebdevelops <sardanhaluciano9690@gmail.com>
2025-02-14 14:20:18 -0300

    user profile ready

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,7 +45,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @access Private
 
 const getMyOrders = asyncHandler(async (req, res) => {
-  const orders = await Order.find({ users: req.user._id });
+  const orders = await Order.find({ user: req.user._id });
   res.status(200).json(orders);
 });
 
