4ea8fbf6222cad43001bb6c3d4cd595e355f196d
lswebdevelops <sardanhaluciano9690@gmail.com>
2025-02-17 22:14:08 -0300

    added edit images to create product screen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -34,7 +34,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: "Sample name",
     price: 0,
     user: req.user._id,
-    image: "images/sample.jgp",
+    image: "/images/sample.jpg",
     brand: "Sample brand",
     category: "Sample category",
     countInStock: 0,
@@ -42,8 +42,38 @@ const createProduct = asyncHandler(async (req, res) => {
     description: "Sample description",
   });
   
-  const createProduct = await product.save();
-  res.status(201).json(createProduct);
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
 });
 
-export { getProducts, getProductById, createProduct };
+
+// @desc update a product
+// @route PUT  /api/product/:id
+// @access private admin
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const {name, price, description, image, brand, category, countInStock } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+
+});
+
+
+
+export { getProducts, getProductById, createProduct, updateProduct };
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL } from "../constants";
+import { PRODUCTS_URL, UPLOAD_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -7,22 +7,46 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       query: () => ({
         url: PRODUCTS_URL,
       }),
-      keepUnusedDataFor: 5
+      providesTags: ["Product"] /** > reloads page */,
+      keepUnusedDataFor: 5,
     }),
     getProductDetails: builder.query({
-        query: (productId) => ({
-            url: `${PRODUCTS_URL}/${productId}`,
-        }),
-        keepUnusedDataFor: 5,
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
     }),
     createProduct: builder.mutation({
       query: () => ({
         url: PRODUCTS_URL,
-        method: 'POST'
+        method: "POST",
       }),
-      invalidatesTags: ['Product'] /* stops it from being cashed (always new data loading to the page) */
-    })
+      invalidatesTags: [
+        "Product",
+      ] /* stops it from being cashed (always new data loading to the page) */,
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Product"] /**cleans cash for later reload */,
+    }),
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `${UPLOAD_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation } = productsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+} = productsApiSlice;
